ce51c6b5fb6a1d5ea0696c938da2b7cf0dc6b84b
tconklin-champlain <timothy.conklin@mymail.champlain.edu>
2021-10-19 02:03:42 +0000

    tried to code first attempt

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -2,4 +2,5 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the rmeote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo -e 'sudo useradd -m -d /home/sys265 -s /bin/bash sys265\nsudo mkdir /home/sys265/.ssh\nsudo cp SYS265/linux/public-keys/id_rsa.pub /home/sys265/.ssh/authorized_keys\nsudo chmod 700 /home/sys265/.ssh\nsudo chmod 600 /home/sys265/.ssh/authorized_keys\nsudo chown -R sys265:sys265 /home/sys265/.ssh'
+
