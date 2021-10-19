dffbce985edcdf7dc92fb33a3ab5ebdff898cb69
tconklin-champlain <timothy.conklin@mymail.champlain.edu>
2021-10-19 01:22:40 +0000

    added /centos7/linux directory and secure ssh script

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,5 @@
+#secure-ssh.sh
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the rmeote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
