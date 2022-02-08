a300077ee3dce9dfc8d7d40cd38bc9d8187c2bec
tconklin-champlain <timothy.conklin@mymail.champlain.edu>
2022-02-08 18:50:57 -0500

    unitl review

--- SYS-320/Week 2/classwork/apacheLog.py
+++ SYS-320/Week 2/classwork/apacheLog.py
@@ -48,3 +48,26 @@ def ssh_pass(filename, searchTerms):
     for eachHost in hosts:
         
         print(eachHost)
+# YAML dissection
+def yaml_pass(filename, service, term):
+    
+    # Call syslogCheck and return the results
+    is_found = logCheck._logs(filename, service, term)
+    
+    # Found list
+    found = []
+    
+    # Loop through results
+    for eachFound in is_found:
+        
+        #Split the results
+        sp_results = eachFound.split(" ")
+        
+        #QQ.exe - tcpconn6.tencent.com:80 close, 0 bytes sent, 0 bytes received, lifetime <1 sec
+        # Append the split value to the found list
+        found.append(sp_results[0] + " " + sp_results[1] + " " + sp_results[3])
+    hosts = set(found)
+    
+    for eachHost in hosts:
+        
+        print(eachHost)
