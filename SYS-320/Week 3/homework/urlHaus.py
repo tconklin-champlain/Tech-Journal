9ef330e0e1d55d56c91332d6ace891818ff9d3a9
T <timothy.conklin@mymail.champlain.edu>
2022-02-10 15:30:44 -0500

    fixed code

--- SYS-320/Week 3/homework/urlHaus.py
+++ SYS-320/Week 3/homework/urlHaus.py
@@ -1,7 +1,7 @@
 # Import re, and sys
 import csv, re, sys
 # Change the serachTerms to contents
-def urlHausOpen(filename,contents):
+def urlHausOpen(filename,searchTerms):
 # Define lists /filename
     filename = "urlHausTestDataSet.csv"
     rows = []
@@ -11,23 +11,17 @@ def urlHausOpen(filename,contents):
         contents = csv.reader(f)
         for row in range(9):
             next(contents)
-            for keyword in contents:
-                    # fix the quotations needed in the function
-                    x = re.findall(r''+keyword+'', contents[2])
-                    for row in contents:
-                        rows.append(row)
-    rows = sorted(rows)
-    return rows
-    # Check to see if there are results
-    if len(contents) == 0:
-        print("No Results")
-        sys.exit(1)
-    # Don't edit this line. It is here to show how it is possible
-    # to remove the "tt" so programs don't convert the malicious
-    # domains to links that an be accidentally clicked on.
-    the_url = eachLine[2].replace("http","hxxp")
-    the_src = eachLine[4]
-    print("""
-    URL:
-    Info: 
-    {}""",format(the_url, the_src,"*"+60))
+        for eachLine in contents:
+            for keyword in searchTerms:
+                # fix the quotations needed in the function
+                x = re.findall(r''+keyword+'', eachLine[2])
+                for _ in x:
+                    # Don't edit this line. It is here to show how it is possible
+                    # to remove the "tt" so programs don't convert the malicious
+                    # domains to links that an be accidentally clicked on.
+                    the_url = eachLine[2].replace("http","hxxp")
+                    the_src = eachLine[6]
+                    print("""
+URL: {}
+Info: {} 
+{}""".format(the_url, the_src,"*"*60))
