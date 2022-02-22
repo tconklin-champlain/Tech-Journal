a34a86dd4689c1181ebe4ee7cbaba9de3a02c4bc
tconklin-champlain <timothy.conklin@mymail.champlain.edu>
2022-02-22 18:43:08 -0500

    Week 4 classwork

---
+++ SYS-320/Week 4/classwork/fs.py
@@ -0,0 +1,70 @@
+# File to traverse a given directory and it's sub directories and retrieve all the files.
+
+from logging import root
+import os, sys
+
+# Get info from commandline
+print(sys.argv)
+
+# Directory to traverse
+rootdir = sys.argv[1]
+
+#print(rootdir)
+
+# We will traverse a directory
+
+# Check if arg is directory
+if not os.path.isdir(rootdir):
+    print("Invalid directory => {}").format(rootdir)
+    exit()
+# List to save files
+fList = []
+
+# Crawl through provided directory
+for root, subfolders, filenames in os.walk(rootdir):
+
+    for f in filenames:
+
+        # print(root + "/" +f)
+        fileList = root + "/" + f
+        #print(fileList)
+        fList.append(fileList)
+
+#print(fList)
+
+def statFile(toStat):
+
+    # i is going to be the variable used for each metadata element
+    i =  os.stat(toStat,follow_symlinks=False)
+
+    # mode
+    mode = i[0]
+
+    # inode
+    inode = i[1]
+
+    # uid
+    uid = i[4]
+
+    #gid
+    gid =i[5]
+
+    #file size
+    fsize = i[6]
+
+    #access time
+    atime = i[7]
+
+    #modification time
+    mtime = i[8]
+
+    #ctime => windows is the birth time of the file, when it was created
+    #unix it is when attributes of file are changed
+    ctime = i[9]
+    crtime = i[9]
+
+    print("0|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|".format(toStat, inode, mode, uid, gid, fsize, atime, mtime, ctime, crtime))
+
+for eachFile in fList:
+
+    statFile(eachFile)
