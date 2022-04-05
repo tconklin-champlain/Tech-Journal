7aa61740c3f5fc3d6d8d5b506638542a2e3b7b62
tconklin-champlain <timothy.conklin@mymail.champlain.edu>
2022-04-05 17:23:06 -0400

    done

--- SYS-320/Week 10/homework/step1.ps1
+++ SYS-320/Week 10/homework/step1.ps1
@@ -1,8 +1,10 @@
 #Create random number to add to file
 $sbRand = get-random -Minimum 1000 -Maximum 9876
+
 # Output name variable
 $outputName =  "D:\Documents\School\Programming\Tech-Journal\SYS-320\Week 10\homework\EnNoB-$sbRand.exe"
 $outputName2 = "C:\Users\knotm\Desktop\Readme.READ"
+
 # Copy powershell.exe to home directory
 Copy-Item "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe" -Destination $outputName
 
@@ -12,9 +14,11 @@ if ( Test-path $outputName ) {
  } else {
      write-host -BackgroundColor "Red" -ForegroundColor "White" "Powershell file was not created!" 
  }
+
  # Creates a Message
  Write-Host "If you want your files restored, please contact me at dunston@champlain.edu.`
 I look forward to doing business with you." > $outputName2
+
 
 # Check if READ file exists
 if ( Test-path $outputName2 ) {
@@ -22,3 +26,6 @@ if ( Test-path $outputName2 ) {
  } else {
      write-host -BackgroundColor "Red" -ForegroundColor "White" "READ file was not created!" 
  }
+
+ # I liked that this assignment showed that malware is someetimes easier to understand then one thinks
+ # No additional questions.
